e8ce54250e43894927593fb726eb118d52a155aa
Asma-IBrahim-j <asmaajarrahemail@gmail.com>
2025-09-08 22:16:34 +0300

    Create Digits.java

---
+++ Digits.java
@@ -0,0 +1,44 @@
+package digits;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author asmaa
+ */
+public class Digits {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner=new Scanner(System.in);
+        int num=scanner.nextInt();
+        long[] array;
+        array = new long[num];
+        for(int i=0;i<num;i++){
+        array[i]=scanner.nextLong();
+        }
+        for(int i=0;i<num;i++){
+        splitdigits(array[i]);
+                   System.out.println("");
+}
+      
+    }
+    public static void splitdigits(long a){
+        int digits =String.valueOf(a).length();
+        
+        for(int i=0;i<digits;i++){   
+       
+          long  x=a%10;
+        System.out.print(x+" ");
+        if(digits>1)a/=10; }
+        }
+
+        }
+        
+        
+    
+    
+
