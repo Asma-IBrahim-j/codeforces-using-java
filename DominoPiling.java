56f4c80b530dcf3385ed828e12f40db370c48cee
Asma-IBrahim-j <asmaajarrahemail@gmail.com>
2025-09-08 21:38:51 +0300

    Create DominoPiling.java

---
+++ DominoPiling.java
@@ -0,0 +1,22 @@
+package domino.piling;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author asmaa
+ */
+public class DominoPiling {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner =new Scanner(System.in);
+        int n=scanner.nextInt();
+        int m=scanner.nextInt();
+        System.out.println(n*m/2);
+    }
+    
+}
