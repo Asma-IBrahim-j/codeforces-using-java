34634e8d9985f96e2e0e0234e55fb32548e32267
Asma-IBrahim-j <asmaajarrahemail@gmail.com>
2025-09-08 22:05:13 +0300

    Create PoliceRecruits.java

---
+++ PoliceRecruits.java
@@ -0,0 +1,28 @@
+package police.recruits;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author asmaa
+ */
+public class PoliceRecruits {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int officers=0;
+        int untreatedcases=0;
+        for(int i=0;i<n;i++){
+        int x=scanner.nextInt();
+        if(x<0){if(officers==0)untreatedcases++;
+        else officers--;}
+        else{officers+=x;}}
+        System.out.println(untreatedcases);
+    }
+    
+}
