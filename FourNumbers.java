ad474a00aa59240736aa5e99c68d7f16cd626a65
Asma-IBrahim-j <asmaajarrahemail@gmail.com>
2025-09-10 22:17:13 +0300

    Create FourNumbers.java

---
+++ FourNumbers.java
@@ -0,0 +1,26 @@
+package fournumbers;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author asmaa
+ */
+public class Fournumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner=new Scanner(System.in);
+        short a =scanner.nextShort();
+        short b=scanner.nextShort();
+        short c=scanner.nextShort();
+        short d=scanner.nextShort();
+        System.out.println((long)a*b*c*d);
+
+
+    }
+    
+}
