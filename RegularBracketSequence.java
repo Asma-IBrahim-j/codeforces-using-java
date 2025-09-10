682e0bf9396b63026889cccd4a64dee8504873bf
Asma-IBrahim-j <asmaajarrahemail@gmail.com>
2025-09-10 22:29:14 +0300

    Create RegularBracketSequence.java

---
+++ RegularBracketSequence.java
@@ -0,0 +1,33 @@
+package regular.bracket.sequence;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author asmaa
+ */
+public class RegularBracketSequence {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner=new Scanner(System.in);
+        String s=scanner.next();
+        char[] ch=s.toCharArray();
+        int counter=0;
+        int size=s.length();
+        for(int i=0;i<s.length();i++){
+        
+        if(ch[i]=='(')counter++;
+        else{
+           
+            counter--;
+            if(counter<0){counter=0;size--;}
+        }
+        }
+        System.out.println(size-counter);
+    }
+    
+}
